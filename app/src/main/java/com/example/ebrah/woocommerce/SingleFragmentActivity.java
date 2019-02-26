f89b8ab5368f66f71b446b11e91c62b88ee04fe1
Thinkin <ebrahim.rhni@gmail.com>
2019-02-26 19:13:46 +0330

    initial commit. Retrofit, RecyclerView and Material Design are added. SingleFragmentActivity is also added

---
+++ app/src/main/java/com/example/ebrah/woocommerce/SingleFragmentActivity.java
@@ -0,0 +1,20 @@
+package com.example.ebrah.woocommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    public abstract Fragment createFragment();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment_activity);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if(fragmentManager.findFragmentById(R.id.single_fragment_container)== null)
+            fragmentManager.beginTransaction().add(R.id.single_fragment_container, createFragment()).commit();
+    }
+}
---
+++ app/src/main/java/com/example/ebrah/woocommerce/WooCommerceActivity.java
@@ -0,0 +1,17 @@
+package com.example.ebrah.woocommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class WooCommerceActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_woo_commerce);
+
+
+    }
+}
